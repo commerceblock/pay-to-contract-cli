b84a3df18db2f7db8e6d2299712201496e0dde23
Nick <nickgregory@nicholass-MacBook-Pro.local>
2017-04-05 20:11:49 -0400

    typo

--- cmds/derive-address-from-path.js
+++ cmds/derive-address-from-path.js
@@ -7,7 +7,7 @@ module.exports = function(program) {
 
   program
     .command('derive-address-from-hash <pubkey> <path>')
-    .description('derive a child address from a given BIP32 comppliant path')
+    .description('derive a child address from a given BIP32 compliant path')
     .action(function(pubkey, path) {
       const hdPublicKey = HDPublicKey.fromString(pubkey)
       const result = hdPublicKey.derive(path).toString()
